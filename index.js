ee86eaeb2ab6491ff4f4123bb15977ddbf885998
Pema Lama <pema.srp72@gmail.com>
2021-09-16 16:39:57 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+
+function writeCards(array, name){
+  let newArr=[];
+  for(let i = 0 ; i< array.length ; i++){
+   let  message= `Thank you, ${array[i]}, for the wonderful ${name} gift!`;
+   newArr.push(message);
+}
+return newArr; 
+}
+
+function countDown(n){
+while(n>-1){
+    console.log(n);
+    n--;
+}
+}
